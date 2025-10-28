9daeee90ccc49e8728c6172e4cc85f17b5013742
Nisha0202 <nishajabatunnessa@gmail.com>
2025-10-28 22:11:35 +0600

    backend connection

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://myportfolio-backend-chi.vercel.app/api",
 });
 
 export default api;
